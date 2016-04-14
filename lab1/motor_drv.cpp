be4052047192be3cfacc7debc75995c17002d675
chrisclash94 <chrisbarth94@yahoo.com>
2016-04-13 19:55:28 -0700

    Finished Motor driver

--- lab1/motor_drv.cpp
+++ lab1/motor_drv.cpp
@@ -1,7 +1,6 @@
 /*************************************************************************************
-/** @file adc.cpp
- *    This file contains a very simple A/D converter driver. This driver should be
- *
+/** @file motor_drv.cpp
+ *    This file contains a very generic motor driver. 
  *  Revisions:
  *    @li 01-15-2008 JRR Original (somewhat useful) file
  *    @li 10-11-2012 JRR Less original, more useful file with FreeRTOS mutex added
@@ -22,7 +21,6 @@
  *    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
  *    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 //*************************************************************************************
-
 #include <stdlib.h>                         // Include standard library header files
 #include <avr/io.h>
 
@@ -35,40 +33,79 @@
  *  @param p_serial_port A pointer to the serial port which writes debugging info. 
  */
 
-motor_drv::motor_drv(emstream* p_serial_port, volatile uint8_t* port_select, volatile uint8_t* pwm_select, uint8_t oc_select) 
-{
-   ptr_to_serial = p_serial_port;
-   volatile uint8_t* p_port = port_select;
-   volatile uint8_t* p_pwm = pwm_select;
+motor_drv::motor_drv(emstream* p_serial_port, 
+		     volatile uint8_t* port_select,      uint8_t port_pin_offset, 
+		     volatile uint8_t* pwm_select,       uint8_t pwm_pin_select) 
+{      
+   ptr_to_serial = p_serial_port;             
+   volatile uint8_t* p_motor_port = port_select;        //Pointer to port on ATmega that connects to motor driver
+   volatile uint8_t* p_motor_dir = (p_motor_port - 1);  //Pointer to I/O register of port
+   volatile uint8_t  pin_shift = port_bitshift ;        //Sets the pin shift for INA, INB, DIAG pins
+   volatile uint8_t* p_pwm = pwm_select;                //Pointer to the PWM Port on ATmega
+   volatile uint8_t* p_pwm_dir = (p_pwm - 1);           //Pointer to the PWM direction register
+   volatile uint8_t OC_XX = pwm_pin_select;             //Selects pin for OCXX (OC1A)
    
+   *p_motor_port &= ~(0b011 << offset);          //Sets INA, INB to 0, enables DIAG (pull up resistor;
+   *p_motor_dir |= (0b111 << offset);            //Sets INA, INB to outputs, DIAG to input
+    
+   *p_pwm_dir &= ~(0x01 << 0C_XX);		//Sets OCXX low
+   *p_pwm |= (0x1 < 0C_XX);                      //Sets OCXX to output
 }
 
-motor_drv:: set_power(int16_t torqe) 
+  
+motor_drv:: set_power(int16_t torque, u_int16_t OCRNO) 
 {
+   u_int8_t duty_cycle; 
+   
    //Checks for a postitive input, which indicates forward motion
    if(torque < 0)
    {
-      INA |= 0x01;
-      INB |= 0x00;
-       
+      *p_port &= ~(0b11 << offset);    // clears INA INB
+      *p_port |= (0b1 << offset);      // sets INA high
    }
    //Checks for negative input, which indicated reverse motion
    else if(torque > 0)
-   {
-       INA = 0x01;
-       INB = 0x00;
-   }
+   { 
+      *p_port &= ~(0b11 << offset);     // clears INA INB
+      *p_port |= (0x10 << offset);     // sets INB hight
+   } 
    //Zero input inidcates no motion
    else
    {
-       INA = 0x00;
-       INB = 0x00;
+      *p_port &= ~(0b11 << offset);     //clears INA INB
    }
-  
+    
+   duty_cycle = abs(torque);
+   
+   switch (OCRNO) 
+   {   
+      case (OCR1A):
+        OCR1A = duty_cycle;
+        break;
+	
+      case (OCR1B):
+        OCR1B = duty_cycle;
+	break;
+
+      case (OCR3A):
+        OCR3A = duty_cycle;
+	break;
+
+      case (OCR3B):
+	OCR3B = duty_cycle;
+	break;
+	
+      case (OCR5A):
+	OCR5A = duty_cycle;
+	break;
+        
+      case (OCR5B):
+	OCR5B = duty_cycle;
+	break;
+   }
 }
 
- motor_drv:: brake(int16_t strength)
+motor_drv:: brake(int16_t strength, u_int16_t OCRNO)
 {
-  
-  
+   set_power(0, OCRNO);
 }
